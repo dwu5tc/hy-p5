6dda61459eb08e027e4a41abdaa5924a3c57b4de
Nahrin Jalal <jalal.nahrin@gmail.com>
2017-06-04 21:47:02 -0400

    stuff updated

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -208,19 +208,35 @@ wineApp.addFilterListener = function() {
 }
 
 
+// //This is for when the wine is "selected"
+// wineApp.addWineSelectionListener = function() {
+// 	$(".wines-inventory").on("click", ".wine-item", function() { // event delegation
+// 		// console.log("SELECTEDYOOOO");
+// 		$(this).toggleClass("wine-item--selected");
+// 		$(this).find("i").toggleClass("hidden");
+// 		if ($(this).hasClass("wine-item--selected")) {
+// 			wineApp.selections.push(this);
+// 		}
+// 		else {
+// 			wineApp.selections.splice(wineApp.selections.indexOf(this), 1);
+// 		}
+// 	});
+// }
+
 //This is for when the wine is "selected"
 wineApp.addWineSelectionListener = function() {
-	$(".wines-inventory").on("click", ".wine-item", function() { // event delegation
-		// console.log("SELECTEDYOOOO");
-		$(this).toggleClass("wine-item--selected");
-		$(this).find("i").toggleClass("hidden");
-		if ($(this).hasClass("wine-item--selected")) {
-			wineApp.selections.push(this);
-		}
-		else {
-			wineApp.selections.splice(wineApp.selections.indexOf(this), 1);
-		}
-	});
+  $(".wines-inventory").on("click", ".wine-item", function() { // event delegation
+    // console.log("SELECTEDYOOOO");
+    $(this).toggleClass("wine-item--selected");
+    $(this).find("i").toggleClass("hidden");
+    if ($(this).hasClass("wine-item--selected")) {
+      wineApp.selections.push(this);
+      wineApp.placeMapMarkers();
+    } else {
+      wineApp.selections.splice(wineApp.selections.indexOf(this), 1);
+      wineApp.placeMapMarkers();
+    }
+  });
 }
 
 
@@ -305,7 +321,7 @@ wineApp.updateWineryList = function() {
 
 //Wine glass marker for winery locations
 wineApp.locationIcon = L.icon({
-	iconUrl: 'assets/wineMarker.svg', // Wine glass image for the map marker
+	iconUrl: 'images/wineMarker.svg', // Wine glass image for the map marker
 	iconSize: [70, 70], // dimensions of the icon
 	iconAnchor:   [15, -5], // point of the icon which will correspond to marker's location
 	popupAnchor: [0, 12.5] // position of the popup relative to the icon
@@ -332,9 +348,15 @@ wineApp.placeMapMarkers = function() {
     var Lng = parseFloat(marker.Lat);
     var wineMarker = L.marker([Lat, Lng], {
       icon: wineApp.locationIcon
-    }).bindPopup(buildMarker(marker)).addTo(wineApp.mymap)
+   }).bindPopup(buildPopup(marker)).addTo(wineApp.mymap)
   });
+
 }
+
+
+
+
+
 
 //Document Ready!!
 $(function(){
